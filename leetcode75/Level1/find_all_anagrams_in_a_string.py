6138f5c347f75ed3223a13765bf87aa1dfd2aa61
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2022-11-29 22:10:59 -0800

    find all anagrams in a string

--- leetcode75/Level1/find_all_anagrams_in_a_string.py
+++ leetcode75/Level1/find_all_anagrams_in_a_string.py
@@ -10,8 +10,8 @@ class Solution:
         if len_p > len_s:
             return []
 
-        p_counter: dict[str, int] = dict(Counter(p))
-        substring_counter: dict[str, int] = {}
+        p_count: dict[str, int] = dict(Counter(p))
+        s_count: dict[str, int] = {}
         found_idx: list[int] = []
 
         # rc: right_pointer_char, lc: left_pointer_char
@@ -20,12 +20,12 @@ class Solution:
 
             if left >= 0:
                 lc = s[left]
-                if lc in substring_counter:
-                    substring_counter[lc] -= 1
+                if lc in s_count:
+                    s_count[lc] -= 1
 
-            if rc in p_counter:
-                substring_counter[rc] = substring_counter.get(rc, 0) + 1
-                if p_counter == substring_counter:
+            if rc in p_count:
+                s_count[rc] = s_count.get(rc, 0) + 1
+                if p_count == s_count:
                     found_idx.append(left + 1) 
 
         return found_idx
